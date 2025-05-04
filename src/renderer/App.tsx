e8bab87add5c850c6664ef7d253b7bcd4b32a137
unknown <arnold.naval99@proton.me>
2025-05-04 16:17:51 -0600

    initial setup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import "tailwindcss/tailwind.css";
 
 function Hello() {
   return (
@@ -8,7 +9,7 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1 className="bg-red">electron-react-boilerplate</h1>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
